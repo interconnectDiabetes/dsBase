4b17aa320795825bf0b9fb5436d7f713f6a83b41
Yannick Marcon <yannick.marcon@obiba.org>
2014-04-04 14:37:14 -0400

    Options key added in DESCRIPTION

--- R/setFilterDS.R
+++ R/setFilterDS.R
@@ -2,12 +2,10 @@
 #' @title Sets the privacy level
 #' @description This is an internal function that set the number of observation
 #' that are allowed in any table or vector
-#' @details The function is called by server side functios
-#' @param x a dummy argument
-#' @return an integer between 1 and 5
+#' @details The function is called by server side functions
+#' @return an integer (default is 5)
 #' @author Gaye, A.
 #'
-.setFilterDS <- function(x=5){
-  a <- x
-  return(5)
+.setFilterDS <- function() {
+  return(as.integer(getOption("datashield.limit", 5)))
 }
